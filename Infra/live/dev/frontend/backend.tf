8213232a432adc7d3b8294e1c97a2fada58c563b
cssedi <u21450511@up.ac.za>
2026-01-18 19:32:50 +0200

    fixed region bug

--- Infra/live/dev/frontend/backend.tf
+++ Infra/live/dev/frontend/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "skillmatcha-terraform-state"
     key            = "live/dev/shared-networking.tfstate"
-    region         = "af-south-1"
+    region         = "eu-central-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
   }
