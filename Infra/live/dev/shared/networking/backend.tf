6a9ddea920cd00298715db1c536b38fb0964c351
cssedi <u21450511@up.ac.za>
2026-01-18 19:01:53 +0200

    removed variables from backend.tf fille

--- Infra/live/dev/shared/networking/backend.tf
+++ Infra/live/dev/shared/networking/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "skillmatcha-terraform-state"
     key            = "live/dev/shared-networking.tfstate"
-    region         = var.aws_region
+    region         = "eu-central-1"
     dynamodb_table = "terraform-state-locks"
     encrypt        = true
   }
